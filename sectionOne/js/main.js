099be346e67b2ee880093bb332f4e1f3b7ab1eb4
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-10 18:33:22 -0500

    putting scroll into function

--- sectionOne/js/main.js
+++ sectionOne/js/main.js
@@ -1,16 +1,17 @@
 //main styles
 $(document).ready(function() {
 
-$('#nav').onePageNav({
-   currentClass: 'current',
-   changeHash: false,
-   scrollSpeed: 750,
-   scrollThreshold: 0.2
-});
 
+	function sectionFlag(){
+		$('#nav').onePageNav({
+		   currentClass: 'current',
+		   changeHash: false,
+		   scrollSpeed: 750,
+		   scrollThreshold: 0.2
+		});
+	}
 
-for( var i = 1; i < 11; i++ ){
-	console.log(i);
-}
+	//fire of on doc.ready
+	sectionFlag();
 
 });
