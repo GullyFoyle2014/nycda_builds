a63410d98e6f2771123a4f341312fba49d21f692
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-10 00:21:52 -0500

    adding class one work files, start of paralexy portfolio site

---
+++ sectionOne/js/main.js
@@ -0,0 +1,12 @@
+//main styles
+$(document).ready(function() {
+
+$('#nav').onePageNav({
+   currentClass: 'current',
+   changeHash: false,
+   scrollSpeed: 750,
+   scrollThreshold: 0.2
+});
+
+
+});
