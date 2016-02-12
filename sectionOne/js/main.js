0f5aaa6a31626cc26a5f5848456e59972de9c13e
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-11 20:46:26 -0500

    adding horizontal nav and breakpoints, changing subnav styles

--- sectionOne/js/main.js
+++ sectionOne/js/main.js
@@ -10,6 +10,30 @@ $(document).ready(function() {
 		   scrollThreshold: 0.2
 		});
 	}
+	function toggleNav(){
+		$('nav .topnav').slideToggle();
+	}
+	//window resize function
+	function windowResized(){
+		var width = $(window).width();
+		var menu = $('nav ul');
+		if( width > 768 && menu.is(':hidden')){
+			menu.removeAttr('style');
+		}
+	}
+	$(window).resize(windowResized);
+
+	//toggles nav on click
+	$('#hamburger').on('click', function(){
+	 	toggleNav();
+	});
+
+	// $(window).resize(function(){
+	// 	var w = $(window).width();
+	// 	if(w > 420 && menu.is('.hidden')){
+	// 		menu.removeAttr('style');
+	// 	}
+	// });
 
 	//fire of on doc.ready
 	sectionFlag();
